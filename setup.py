af947b0db1fd4d88f2b978114b30f627c1e2c808
dosisod <39638017+dosisod@users.noreply.github.com>
2023-03-28 22:09:30 -0700

    Move some fieds from `setup.py` to `pyproject.toml`

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from distutils.core import setup, Extension
 
 def main():
     setup(
-        name="squall",
-        version="1.0.0",
         ext_modules=[
             Extension("squall", ["squall/squall.c"], libraries=["sqlite3"]),
         ],
